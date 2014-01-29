c932f16457019fe83cce72879a12f25245d53369
bloodyjoni <diguet.jonathan@gmail.com>
2014-01-29 09:42:52 -0300

    Init SCTyPE la Matanza v4 with SCTyPE laMatanza v3 code 29/01/2013

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class Snippet {
+	touch README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git remote add origin https://github.com/bloodyjoni/SCTyPElaMatanzav4.git
+	git push -u origin master
+}
+
